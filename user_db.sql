60ac5ebf1dcb28fa63a8d8112c0ac3c50f87f4ef
Nguyen Thai Sang <sang.nguyen@php-sangnguyen.sutrixmedia1.com>
2015-10-14 17:24:25 +0700

    add sql fiel

--- user_db.sql
+++ user_db.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Oct 13, 2015 at 05:16 PM
+-- Generation Time: Oct 14, 2015 at 05:22 PM
 -- Server version: 5.5.44-0ubuntu0.14.04.1
 -- PHP Version: 5.5.9-1ubuntu4.13
 
@@ -41,7 +41,57 @@ INSERT INTO `company` (`company_id`, `vi_name`, `en_name`) VALUES
 (1, 'Đại Hàn', 'Korea'),
 (2, 'Nhật Bản', 'Japan'),
 (3, 'Mỹ', 'USA'),
-(4, 'Mã Lai Á', 'Malaysia');
+(4, 'Mã Lai', 'Malaysia');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `group`
+--
+
+CREATE TABLE IF NOT EXISTS `group` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) CHARACTER SET utf8 NOT NULL,
+  `description` varchar(250) CHARACTER SET utf8 DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+
+--
+-- Dumping data for table `group`
+--
+
+INSERT INTO `group` (`ID`, `name`, `description`) VALUES
+(1, 'Super Admin', 'Super Admin'),
+(2, 'Company leader', 'Company team leader'),
+(3, 'Company creater', 'Company creater'),
+(4, 'User editer', 'User editer');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `group_resource`
+--
+
+CREATE TABLE IF NOT EXISTS `group_resource` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `groupID` int(11) NOT NULL,
+  `resourceID` int(11) NOT NULL,
+  `create` tinyint(1) NOT NULL,
+  `edit` tinyint(1) NOT NULL,
+  `delete` tinyint(1) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+
+--
+-- Dumping data for table `group_resource`
+--
+
+INSERT INTO `group_resource` (`ID`, `groupID`, `resourceID`, `create`, `edit`, `delete`) VALUES
+(1, 1, 1, 1, 1, 1),
+(2, 2, 2, 1, 1, 1),
+(3, 1, 2, 1, 1, 1),
+(4, 3, 2, 1, 0, 0),
+(5, 4, 1, 0, 1, 0);
 
 -- --------------------------------------------------------
 
@@ -54,6 +104,29 @@ CREATE TABLE IF NOT EXISTS `permission` (
   `name` varchar(50) CHARACTER SET utf8 NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `resource`
+--
+
+CREATE TABLE IF NOT EXISTS `resource` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `resource_name` varchar(40) CHARACTER SET utf8 NOT NULL,
+  `resource_description` varchar(250) CHARACTER SET utf8 NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+
+--
+-- Dumping data for table `resource`
+--
+
+INSERT INTO `resource` (`ID`, `resource_name`, `resource_description`) VALUES
+(1, 'user', 'user'),
+(2, 'company', 'user'),
+(3, 'group', 'user'),
+(4, 'perms', 'user');
 
 -- --------------------------------------------------------
 
@@ -72,7 +145,7 @@ CREATE TABLE IF NOT EXISTS `user` (
   `gender` tinyint(1) DEFAULT NULL,
   `companyid` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=13 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=14 ;
 
 --
 -- Dumping data for table `user`
@@ -85,8 +158,32 @@ INSERT INTO `user` (`id`, `username`, `password`, `email`, `permission`, `dob`, 
 (8, 'usertest2', '202cb962ac59075b964b07152d234b70', 'usertest2@mail.com', 1, '1997-05-03', 1, 1, 1),
 (9, 'usertest3', '202cb962ac59075b964b07152d234b70', 'usertest@mail.com', 0, '2000-06-04', 1, 1, 4),
 (10, 'usertest4', 'usertest', 'usertest4@mail.com', 0, '1990-12-25', 1, 1, 1),
-(11, 'root', 'e10adc3949ba59abbe56e057f20f883e', 'root@gmail.com', 0, '2004-02-02', 1, 1, 2),
-(12, 'kira', '202cb962ac59075b964b07152d234b70', 'kira@light.com', 0, '2000-03-03', 1, 1, 2);
+(11, 'root', 'e10adc3949ba59abbe56e057f20f883e', 'root@gmail.com', 1, '2000-03-03', 1, 1, 1),
+(12, 'kira', '202cb962ac59075b964b07152d234b70', 'kira@light.com', 0, '1997-07-04', 1, 1, 1),
+(13, 'moon', '202cb962ac59075b964b07152d234b70', 'moon@mym.com', 1, '1992-06-01', 1, 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_group`
+--
+
+CREATE TABLE IF NOT EXISTS `user_group` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `userID` int(11) NOT NULL,
+  `groupID` int(11) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+
+--
+-- Dumping data for table `user_group`
+--
+
+INSERT INTO `user_group` (`ID`, `userID`, `groupID`) VALUES
+(1, 1, 1),
+(2, 11, 3),
+(3, 12, 2),
+(4, 13, 4);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
