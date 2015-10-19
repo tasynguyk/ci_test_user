277519919c0dca2e7e37d169801f965b3edd4ecc
Nguyen Thai Sang <sang.nguyen@php-sangnguyen.sutrixmedia1.com>
2015-10-19 16:35:42 +0700

    new sql file

--- user_db.sql
+++ user_db.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Oct 16, 2015 at 01:31 PM
+-- Generation Time: Oct 19, 2015 at 04:28 PM
 -- Server version: 5.5.44-0ubuntu0.14.04.1
 -- PHP Version: 5.5.9-1ubuntu4.13
 
@@ -27,21 +27,43 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `company` (
-  `company_id` int(11) NOT NULL AUTO_INCREMENT,
-  `vi_name` varchar(40) CHARACTER SET utf8 NOT NULL,
-  `en_name` varchar(40) CHARACTER SET utf8 NOT NULL,
-  PRIMARY KEY (`company_id`)
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
 
 --
 -- Dumping data for table `company`
 --
 
-INSERT INTO `company` (`company_id`, `vi_name`, `en_name`) VALUES
-(1, 'Đại Hàn', 'Korea'),
-(2, 'Nhật Bản', 'Japan'),
-(3, 'Mỹ', 'USA'),
-(4, 'Mã Lai', 'Malaysia');
+INSERT INTO `company` (`id`) VALUES
+(1),
+(2),
+(4);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `company_name`
+--
+
+CREATE TABLE IF NOT EXISTS `company_name` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `company_id` int(11) NOT NULL,
+  `language` varchar(45) NOT NULL,
+  `name` varchar(45) CHARACTER SET utf8 DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+
+--
+-- Dumping data for table `company_name`
+--
+
+INSERT INTO `company_name` (`id`, `company_id`, `language`, `name`) VALUES
+(1, 1, 'vietnamese', 'Đại Hàn'),
+(2, 2, 'english', 'abc'),
+(4, 1, 'english', 'Korea'),
+(5, 4, 'english', 'USA'),
+(6, 4, 'vietnamese', 'Hoa Kỳ');
 
 -- --------------------------------------------------------
 
@@ -95,6 +117,7 @@ INSERT INTO `group_resource` (`ID`, `groupID`, `resourceID`, `create`, `edit`, `
 (7, 1, 4, 1, 1, 1),
 (9, 2, 2, 1, 1, 1);
 
+-- -------------------------------------------------------
 -- --------------------------------------------------------
 
 --
@@ -155,15 +178,12 @@ CREATE TABLE IF NOT EXISTS `user` (
 
 INSERT INTO `user` (`id`, `username`, `password`, `email`, `permission`, `dob`, `status`, `gender`, `companyid`) VALUES
 (1, 'sangnguyen', 'e10adc3949ba59abbe56e057f20f883e', 'sang.nguyen@gmail.com', 2, '1992-12-25', 1, 1, 1),
-(5, 'user2', '202cb962ac59075b964b07152d234b70', 'tzz@gmail.com', 0, '2005-03-01', 1, 1, 3),
-(7, 'ausertest1', '123456', 'usertest1@mail.com', 0, '1995-09-20', 1, 1, 2),
+(7, 'ausertest1', '123456', 'usertest1@mail.com', 0, '1995-09-20', 1, 1, 1),
 (8, 'usertest2', '202cb962ac59075b964b07152d234b70', 'usertest2@mail.com', 1, '1997-05-03', 1, 1, 1),
-(9, 'usertest3', '202cb962ac59075b964b07152d234b70', 'usertest@mail.com', 0, '2000-06-04', 1, 1, 4),
+(9, 'usertest3', '202cb962ac59075b964b07152d234b70', 'usertest@mail.com', 0, '2000-06-04', 1, 1, 2),
 (10, 'usertest4', 'usertest', 'usertest4@mail.com', 0, '1990-12-25', 1, 1, 1),
 (11, 'root', 'e10adc3949ba59abbe56e057f20f883e', 'root@gmail.com', 1, '2000-03-03', 1, 1, 1),
-(12, 'kira', '202cb962ac59075b964b07152d234b70', 'kira@light.com', 1, '1997-07-04', 1, 1, 1),
-(13, 'moon', '202cb962ac59075b964b07152d234b70', 'moon@mym.com', 1, '1992-06-01', 1, 1, 1),
-(14, 'admin', '202cb962ac59075b964b07152d234b70', '123456@dfgdf.vc', 0, '2003-02-02', 1, 1, 1);
+(13, 'moon', '202cb962ac59075b964b07152d234b70', 'moon@mym.com', 1, '1992-06-01', 1, 1, 1);
 
 -- --------------------------------------------------------
 
@@ -176,7 +196,7 @@ CREATE TABLE IF NOT EXISTS `user_group` (
   `userID` int(11) NOT NULL,
   `groupID` int(11) NOT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=11 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=9 ;
 
 --
 -- Dumping data for table `user_group`
